196e5d42396af6a59d452078a9ad97da24c2642f
andrewshayne <andrewshayne@gmail.com>
2020-10-03 02:12:08 -0500

    added some physics, projectiles, animations...

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,13 @@ import TileMapScene from './scenes/TileMapScene'
 
 const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
+	width: 1280,
+	height: 720,
 	physics: {
 		default: 'arcade',
 		arcade: {
-			gravity: { y: 200 }
+			debug: true,
+			gravity: { y: 1200 }
 		}
 	},
 	scene: [TileMapScene, HelloWorldScene] //looks like it defaults to the first scene in the list
--- src/scenes/TileMapScene.ts
+++ src/scenes/TileMapScene.ts
@@ -1,71 +1,190 @@
 import Phaser, { GameObjects } from 'phaser'
 
+
+enum action {
+    JUMP = 1,
+    FIRE
+}
+
+//player
 let player: Phaser.Physics.Arcade.Sprite
+let isJumping: boolean
+let isKnockback: boolean
+let knockbackTimer: Phaser.Time.TimerEvent
+
+//layers
+let layer1: Phaser.Tilemaps.StaticTilemapLayer
+let layer2: Phaser.Tilemaps.StaticTilemapLayer
+let layer3: Phaser.Tilemaps.StaticTilemapLayer
+
+//player timers
+
+
+//player projectiles
+let projectiles: Phaser.GameObjects.Group
+
+//keyboard variables
+let key_space: Phaser.Input.Keyboard.Key
+let key_e: Phaser.Input.Keyboard.Key
+
+//audio
+let fire_sound: Phaser.Sound.BaseSound
+let jump_sound: Phaser.Sound.BaseSound
 
 export default class TileMapScene extends Phaser.Scene
 {
 	constructor()
 	{
 		super('tilemap-scene')
-	}
-
+    }
+    
 	preload()
     {
+        this.load.image('bg', 'bg.png')
         this.load.image('tiles', 'assetTiles.png')
         this.load.tilemapTiledJSON('map', 'assetTiles.json')
-        this.load.spritesheet('fox_animation', 'foxrun.png', { frameWidth: 110, frameHeight: 128 })
+        this.load.spritesheet('fox_animation', 'magerun192.png', { frameWidth: 192, frameHeight: 206 })
+        this.load.spritesheet('fireball_animation', 'fireball.png', { frameWidth: 55, frameHeight: 55 })
+
+        //audio
+        this.load.audio('fire_sfx', 'sfx/fireballs/Fireball1.wav')
+        this.load.audio('jump_sfx', 'sfx/landing/Landing.wav')
     }
 
     create()
     {
+        //set bg
+        this.add.image(0, 0, 'bg')
+
         const map = this.make.tilemap({key:'map'})
         const tileset = map.addTilesetImage('tileset', 'tiles') //grab the tiled tileset file "tileset" from "tiles" image file
 
-        const layer1 = map.createStaticLayer('Tile Layer 1', tileset, 0, 0)
-        const layer2 = map.createStaticLayer('Tile Layer 2', tileset, 0, 0)
-        const layer3 = map.createStaticLayer('Tile Layer 3', tileset, 0, 0)
+        layer1 = map.createStaticLayer('Tile Layer 1', tileset, 0, 0)
+        layer2 = map.createStaticLayer('Tile Layer 2', tileset, 0, 0)
+        layer3 = map.createStaticLayer('Tile Layer 3', tileset, 0, 0)
 
-        ///
+
+        //map.setCollisionByProperty({ collides: true })
+
+        ///player
         player = this.physics.add.sprite(100, 450, 'fox_animation')
-        player.setBounce(0.2)
+        //player.setBounce(0.2)
+        //player.setFrictionX(0.9)
+        //player.setMaxVelocity(500, 500)
         player.setCollideWorldBounds(true) //collide with world bounds
 
+        //set player bools
+        isJumping = false
+        isKnockback = false
+
+        //create projectile group
+        projectiles = this.physics.add.group({key: 'projectiles', immovable: true, allowGravity: false})
+        projectiles.createMultiple({ classType: Phaser.Physics.Arcade.Sprite, quantity: 5, active: false, visible: false, key: 'fireball_animation' })
+
+
+        //colliders???
+        this.physics.add.collider(player, layer1)
+        this.physics.add.collider(player, layer2)
+        this.physics.add.collider(player, layer3)
+
+        //player animations
         this.anims.create({
-            key: 'right',
-            frames: this.anims.generateFrameNumbers('fox_animation', { start: 0, end: 6 }),
+            key: 'playerMove',
+            frames: this.anims.generateFrameNumbers('fox_animation', { frames: [0,0,1,1,2,2,2,3,3,4,4,5,5,5] }),
+            frameRate: 22,
+            repeat: -1
+        })
+        this.anims.create({
+            key: 'playerStill',
+            frames: this.anims.generateFrameNumbers('fox_animation', { start: 1, end: 1})
+        })
+
+        //projectile animations
+        this.anims.create({
+            key: 'fire',
+            frames: this.anims.generateFrameNumbers('fireball_animation', { start: 0, end: 6 }),
             frameRate: 10,
             repeat: -1
         })
 
-        this.anims.create({
-            key: 'still',
-            frames: this.anims.generateFrameNumbers('fox_animation', { start: 2, end: 2})
-        })
         ///
 
+
+        //keys
+        key_space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
+        key_e = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E)
+
+        //audio
+        fire_sound = this.sound.add('fire_sfx')
+        jump_sound = this.sound.add('jump_sfx')
     }
 
     update(time, delta)
     {
 
         const cursors = this.input.keyboard.createCursorKeys()
-        const runVelocity = 150
-        if(cursors.right?.isDown) {
-            player.setVelocityX(runVelocity)
+        const runVelocity = 100
+        const maxVel = 300
+        const knockbackVelocity = 2000
+        const jumpVelocity = 500
+
+        player.setVelocityX(player.body.velocity.x * 0.9)
+        
+        //no horizontal movement
+        if(!cursors.right?.isDown && !cursors.left?.isDown) {
+            //could decide in here to play idle if velocity is 0, or slowdownrun if velocity != 0
+            player.setVelocityX(player.body.velocity.x * 0.9)
+            player.anims.play('playerStill', true)
+        }
+        //move right
+        if(cursors.right?.isDown && !isKnockback) {
+            player.setVelocityX(Phaser.Math.Clamp(player.body.velocity.x + runVelocity, -maxVel, maxVel))
             player.flipX = false;
-            player.anims.play('right', true)
+            player.anims.play('playerMove', true)
         }
-        else if(cursors.left?.isDown) {
-            player.setVelocityX(-runVelocity)
+        //move left
+        if(cursors.left?.isDown && !isKnockback) {
+            player.setVelocityX(Phaser.Math.Clamp(player.body.velocity.x - runVelocity, -maxVel, maxVel))
             player.flipX = true;
-            player.anims.play('right', true)
+            player.anims.play('playerMove', true)
         }
-        else {
-            player.setVelocityX(0)
-            player.anims.play('still', true)
+        //jump - ADD TIMER!!!
+        if(Phaser.Input.Keyboard.JustDown(key_space)) {
+            isJumping = true
+            player.setVelocityY(-jumpVelocity)
+            jump_sound.play()
+        }
+        //strong attack
+        if(Phaser.Input.Keyboard.JustDown(key_e)) {
+            //spawn projectile in direction we are facing, apply set knockback to character
+            this.fireProjectile(player.x, player.y)
+            if(player.flipX)
+                player.setVelocityX(knockbackVelocity)
+            else
+                player.setVelocityX(-knockbackVelocity)
+            fire_sound.play()
+            knockbackTimer = this.time.delayedCall(400, this.unlockPlayerMovement) //disallow player input for x milliseconds
+            isKnockback = true
         }
 
+    }
 
+    fireProjectile(x: integer, y: integer) {
+        //could run out... but should always have one available!
+        const fireVelocity = 600
+        const p : Phaser.Physics.Arcade.Sprite = projectiles.getFirstDead()
+        p.body.reset(x, y)
+        p.setActive(true)
+        p.setVisible(true)
+        if(player.flipX)
+            p.setVelocityX(-fireVelocity)
+        else
+            p.setVelocityX(fireVelocity)
+        p.anims.play('fire', true)
+    }
+
+    unlockPlayerMovement() {
+        isKnockback = false
     }
 }
 
