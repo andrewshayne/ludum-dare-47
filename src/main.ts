eca14ae1b94dee2f7bf63fba0ae7cedf534d28a2
andrewshayne <andrewshayne@gmail.com>
2020-10-04 06:17:28 -0500

    good morning bic, I feel dead inside

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,15 @@ const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
 	width: 1280,
 	height: 720,
+	audio: {
+		disableWebAudio: true,
+		noAudio: false
+	}
 	physics: {
 		default: 'arcade',
 		arcade: {
-			debug: false,
-			gravity: { y: 1400 }
+			debug: true,
+			gravity: { y: 1600 }
 		}
 	},
 	scene: [TileMapScene, HelloWorldScene] //looks like it defaults to the first scene in the list
